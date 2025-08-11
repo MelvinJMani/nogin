e0cf48153075b29ec19350c7698694e4d7c7586a
Melvin Joseph Mani <melvin.mani@git.ch>
2025-08-11 17:09:53 +0530

    feat: test case fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders nogin title', () => {
+test('renders app title', () => {
   render(<App />);
-  expect(screen.getByText(/nogin/i)).toBeInTheDocument();
+  const title = screen.getByRole('heading', { name: /nogin|vite \+ react/i });
+  expect(title).toBeInTheDocument();
 });
